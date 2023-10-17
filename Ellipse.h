77586639105a7621606fcdd164aca2e1565d70fb
FrenkenFlores <saifualdin.evloev@gmail.com>
2023-10-17 11:59:07 +0300

    update #4: add comments to the Ellipse class

--- Ellipse.h
+++ Ellipse.h
@@ -12,13 +12,17 @@
 
 class Ellipse : public Curve {
 private:
+    // Define the fixed points of an Ellipse.
     double a;
     double b;
+    // Set the cononical class structure.
     Ellipse(){}
     Ellipse(const Ellipse& e) {}
     Ellipse& operator=(const Ellipse& e);
 public:
+    // Add the constructor that initialize the ellipse fixed points.
     Ellipse(double a, double b): a(a), b(b) {}
+    // Implmenet the getPoint method.
     Point getPoint(const double t) const override {
         return {
             a * cos(t), // The x coordinate value.
@@ -26,6 +30,7 @@ public:
             0 // The z coordinate value.
         };
     }
+    // Implement the getDerivative method.
     Point getDerivative(const double t) const override {
         return {
             a * -sin(t), // The x coordinate value.
